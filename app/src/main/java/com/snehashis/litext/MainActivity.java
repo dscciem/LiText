b2713f16838247a46eb985ec39154fdce04ab4fb
snehashis365 <snehashis.2000@gmail.com>
2020-09-14 02:13:01 +0530

    Just adjusting the layout added auto dark mode Main logic will come later just setting up for now

--- app/src/main/java/com/snehashis/litext/MainActivity.java
+++ app/src/main/java/com/snehashis/litext/MainActivity.java
@@ -2,13 +2,18 @@ package com.snehashis.litext;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.HapticFeedbackConstants;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText userInput;
+    EditText userInput, fileName;
+    Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         userInput = findViewById(R.id.userInput);
+        fileName = findViewById(R.id.fileName);
+        saveButton = findViewById(R.id.saveButton);
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "File Save Clicked!! Saved as: '" + fileName.getText().toString() +"'", Toast.LENGTH_SHORT).show();
+                v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+            }
+        });
     }
 }
