117a6a216eaa1d0bb6286a762336e2039e148d1a
snehashis365 <snehashis.2000@gmail.com>
2020-09-14 00:45:12 +0530

    Switched to Relative Layout and added text field

--- app/src/main/java/com/snehashis/litext/MainActivity.java
+++ app/src/main/java/com/snehashis/litext/MainActivity.java
@@ -2,12 +2,20 @@ package com.snehashis.litext;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.userInput);
+
     }
 }
